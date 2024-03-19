e13e126ba8bfd55895a85551a46322f6a81f469b
Dylan Manuel <dylan.manuel@utsa.edu>
2024-03-18 22:36:26 -0500

    load/create LLM objects

--- src/llmftg/cli.py
+++ src/llmftg/cli.py
@@ -1,11 +1,11 @@
 '''
 Command line interface functionality.
 '''
+import shutil
 
-from enum import Enum
 from pathlib import Path
 from typing import Annotated
-from typer import Argument, Option, Typer
+from typer import Argument, BadParameter, Option, Typer
 
 from llmftg.llm import LLM, SupportedModel
 
@@ -16,5 +16,17 @@ Main CLI app.
 
 
 @app.command()
-def command(models: Annotated[Path, Argument(file_okay=False)]) -> None:
-    print(LLM(models, SupportedModel.LLAMA))
+def command(models: Annotated[Path, Argument(file_okay=False)],
+            retrain: Annotated[bool, Option()] = False) -> None:
+    if retrain:
+        shutil.rmtree(models)
+    if not models.exists():
+        models.mkdir(exist_ok=True, parents=True)
+        llms = [LLM(models / m.directory_name, m) for m in SupportedModel]
+    else:
+        llms = [LLM.from_pretrained(p) for p in models.glob('*')]
+        if len(llms) < len(SupportedModel):
+            raise BadParameter(f'Expected {len(SupportedModel)} models in {models}. ' +
+                               'Use --retrain to clear the directory',
+                               param_hint='models')
+    print(llms)
